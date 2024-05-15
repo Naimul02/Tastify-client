05f06f30a1b552b2e0c00a6300ce08b08fe994c3
Naimul Islum <naimulislum39@gmail.com>
2024-05-15 13:04:19 +0600

    commit no 15

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,6 +14,7 @@ const Home = () => {
       </Helmet>
       {/* /banner */}
       <Banner></Banner>
+      {/* foods */}
       <Foods></Foods>
       <OurRestaurant></OurRestaurant>
       <Contact></Contact>
